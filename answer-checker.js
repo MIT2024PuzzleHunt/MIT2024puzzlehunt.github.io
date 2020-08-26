02f530161e0c308be9d2cf2fe45fc0cff4972ca0
jonsh <jonsh@mit.edu>
2020-08-25 21:26:39 -0400

    Add answer checker to all pages

--- answer-checker.js
+++ answer-checker.js
@@ -18,6 +18,29 @@ function hashAnswerTest(submission) {
 function checkAnswer(puzzleId, submission) {
     const answerHashCodes = {
         'arts-1': 5,
+        'arts-2': 5,
+        'arts-3': 5,
+        'arts-4': 5,
+        'ers-1': 5,
+        'ers-2': 5,
+        'ers-3': 5,
+        'ers-4': 5,
+        'ers-5': 5,
+        'ers-6': 5,
+        'hum-1': 5,
+        'hum-2': 5,
+        'hum-3': 5,
+        'hum-4': 5,
+        'int-1': 5,
+        'int-2': 5,
+        'int-3': 5,
+        'int-4': 5,
+        'int-5': 5,
+        'ss-1': 5,
+        'ss-2': 5,
+        'ss-3': 5,
+        'ss-4': 5,
+        'ss-5': 5
     }    
     const answerHashCode = answerHashCodes[puzzleId];    
     if (hashAnswerTest(submission) == answerHashCode) {
